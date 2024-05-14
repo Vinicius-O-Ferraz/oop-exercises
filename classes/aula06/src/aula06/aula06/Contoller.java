aa6d65ff3c0828193d7c8bbdeaab85e2fa9efdc9
Vinícius Ferreira Ferraz <146992032+Vinicius-O-Ferraz@users.noreply.github.com>
2024-05-14 16:46:30 -0300

    Update Contoller.java

--- classes/aula06/src/aula06/aula06/Contoller.java
+++ classes/aula06/src/aula06/aula06/Contoller.java
@@ -1,2 +1,16 @@
 package aula06;
 
+public interface Contoller {
+
+    public abstract void turnOn();
+    public abstract void turnOff();
+    public abstract void openMenu();
+    public abstract void closeMenu();
+    public abstract void increaseVolume();
+    public abstract void decreaseVolume();
+    public abstract void mute();
+    public abstract void unmute();
+    public abstract void play();
+    public abstract void pause();
+
+}
