9be89493d77df44d30f23c2f81a25fda2baa3aa6
Vinícius Ferreira Ferraz <146992032+Vinicius-O-Ferraz@users.noreply.github.com>
2024-05-14 17:19:49 -0300

    Update RemoteController.java

--- classes/aula06/src/aula06/aula06/RemoteController.java
+++ classes/aula06/src/aula06/aula06/RemoteController.java
@@ -2,4 +2,38 @@ package aula06;
 
 public class RemoteController{    
    
+    private int volume;
+    private boolean on;
+    private boolean playing;
+
+
+    public RemoteController(){
+        this.volume = 50;
+        this.on = false;
+        this.playing = false;
+    }
+
+    private int getVolume(){
+        return this.volume;
+    }
+
+    private boolean getOn(){
+        return this.on;
+    }
+
+    private boolean getPlaying(){
+        return this.playing;
+    }
+
+    private void setVolume(int volume){
+        this.volume = volume;
+    }
+
+    private void setOn(boolean on){
+        this.on = on;
+    }
+
+    private void setPlaying(boolean playing){
+        this.playing = playing;
+    }
 }
