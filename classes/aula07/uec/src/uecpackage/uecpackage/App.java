db092cfc0c9f3f68d192e0686c092d46a044539d
Vinícius Ferreira Ferraz <146992032+Vinicius-O-Ferraz@users.noreply.github.com>
2024-05-15 21:49:11 -0300

    Add files via upload

---
+++ classes/aula07/uec/src/uecpackage/uecpackage/App.java
@@ -0,0 +1,11 @@
+package uecpackage;
+
+    public class App {
+        public static void main(String[] args) throws Exception {
+            Fighter f = new Fighter("Mãozinha","Braileiro",24,1.75f,69,12,4,5);
+            f.presentation();
+    }
+}
+    
+
+
